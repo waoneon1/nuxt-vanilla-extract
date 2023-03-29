dc69b2e853e2fa4e5b94785879e8485857a92156
Dharmawan Sukma <waone.on1@gmail.com>
2023-03-29 10:07:15 +0700

    Initial commit

---
+++ CHANGELOG.md
@@ -0,0 +1,28 @@
+# example
+
+## 0.1.3
+
+### Patch Changes
+
+- Updated dependencies [ce2f0ce]
+  - rainbow-sprinkles@0.8.1
+
+## 0.1.2
+
+### Patch Changes
+
+- Updated dependencies [abbd196]
+- Updated dependencies [f327cba]
+  - rainbow-sprinkles@0.8.0
+
+## 0.1.1
+
+### Patch Changes
+
+- 271d262: Introduced staticProperties, which allows configuration of fixed utility classes (not dynamic). Can be useful for frequently used CSS property/value pairs to reduce overall bundle size
+
+  - `properties` renamed to `dynamicProperties`
+
+- Updated dependencies [271d262]
+- Updated dependencies [80821d8]
+  - rainbow-sprinkles@0.7.0
---
+++ pages/index.tsx
@@ -0,0 +1,45 @@
+import "../styles/index.css";
+import { Box } from "../components/Box";
+import { Pinwheel } from "../components/Pinwheel";
+
+export default function App() {
+  return (
+    <Box
+      bg="$core60"
+      height="100vh"
+      width="100vw"
+      display="flex"
+      placeItems="center"
+      padding="$3500"
+      fontFamily="$body"
+    >
+      <Box
+        bg="$core10"
+        borderRadius={{ mobile: "$4x", desktop: "$5x" }}
+        padding={{ mobile: "$2500", desktop: "$3000" }}
+      >
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          gap={{ mobile: "$2000", desktop: "$2500" }}
+        >
+          <Box
+            animation={{ hover: "5s pinwheelSpin ease infinite" }}
+            size={{ mobile: "60px", tablet: "75px", desktop: "100px" }}
+            aria-hidden="true"
+          >
+            <Pinwheel label="Wayfair Pinwheel" />
+          </Box>
+          <Box
+            as="h1"
+            color="$black80"
+            typeSize={{ mobile: "$1000", desktop: "$1000" }}
+          >
+            Vanilla Extract & Rainbow Sprinkles
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
